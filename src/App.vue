ffb11b0f3a0b8184c0f8791eade971b2bc794134
lituping <l13608127035@163.com>
2021-10-08 17:40:24 +0800

    引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
   <div id="app">
     <router-view />
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-guanbi"></i>
+    </div>
+    <div>
+      <van-button type="primary">主要按钮</van-button>
+      <van-button type="info">信息按钮</van-button>
+    </div>
   </div>
 </template>
 <script>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-// 加载全局样式
 import './styles/index.less'
+// 加载vant核心组件库
+import Vant from 'vant'
+// 加载vant 全局样式
+import 'vant/lib/index.css'
+// 注册使用vant
+Vue.use(Vant)
+// 加载全局样式
+
 Vue.config.productionTip = false
 
 new Vue({
